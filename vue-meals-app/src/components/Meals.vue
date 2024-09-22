96f0aa7966d652f0a55184a73f74965d378237ef
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-09-22 15:13:30 +0200

    init + get meals from API

---
+++ vue-meals-app/src/components/Meals.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  meals: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h2>Meals</h2>
+    <ul>
+      <li v-for="meal in meals" :key="meal.id">
+        <a :href="meal.url">{{ meal.strMeal }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vue-meals-app/src/views/Home.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Meals from "@/components/Meals.vue";
+import { onMounted, ref } from "vue";
+
+const meals = ref([]);
+
+onMounted(() => {
+  for (let i = 0; i < 10; i++) {
+    fetch("https://themealdb.com/api/json/v1/1/random.php")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        meals.value.push(data.meals[0]);
+      });
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>Random Meals</h1>
+  </div>
+  <Meals :meals="meals" />
+</template>
+
+<style scoped></style>
